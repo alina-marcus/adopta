c7734acf0eb6cfc1373662f620c5dea0612c7bf9
Alina Marcus <alinamarcus@Mac.fritz.box>
2025-08-26 11:01:12 +0200

    Initial setup flask app & dog database

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Models importieren
+    from models import Dog
+
+    # Routes registrieren
+    from routes import dogs_bp
+    app.register_blueprint(dogs_bp, url_prefix="/dogs")
+
+    return app
+
+# Flask CLI braucht diese Variable
+app = create_app()
---
+++ routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from app import db
+from models import Dog
+
+dogs_bp = Blueprint("dogs", __name__)
+
+@dogs_bp.route("/", methods=["GET"])
+def get_dogs():
+    dogs = Dog.query.all()
+    return jsonify([{
+        "dog_id": d.dog_id,
+        "dog_name": d.dog_name,
+        "dog_gender": d.dog_gender,
+        "dog_available_for_adoption": d.dog_available_for_adoption
+    } for d in dogs])
+
+@dogs_bp.route("/", methods=["POST"])
+def add_dog():
+    data = request.json
+    dog = Dog(
+        dog_name=data["dog_name"],
+        dog_gender=data["dog_gender"],
+        dog_available_for_adoption=data.get("dog_available_for_adoption", True)
+    )
+    db.session.add(dog)
+    db.session.commit()
+    return jsonify({"message": "Dog added", "dog_id": dog.dog_id})
